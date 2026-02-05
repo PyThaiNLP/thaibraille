0e8f5b625c5816b3aeaa89148c3b88303abd64f1
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2026-02-05 14:40:22 +0700

    Add test for thai_word_braille with 'เรียน'

--- tests/test_text.py
+++ tests/test_text.py
@@ -8,6 +8,7 @@ class TestTextPackage(unittest.TestCase):
         self.assertEqual(thai_word_braille("ลิ้น"), '⠇⠃⠲⠝')
         self.assertEqual(thai_word_braille("ว่าง"), '⠺⠔⠡⠻')
         self.assertEqual(thai_word_braille("แก้ม"), '⠣⠛⠲⠍')
+        self.assertEqual(thai_word_braille("เรียน"), '⠗⠷⠝')
 
     def test_thai_text_braille(self):
         self.assertEqual(thai_text_braille("แมวกิน   ปลา"), ['⠣⠍⠺', '⠛⠃⠝', '   ', '⠯⠇⠡'])
